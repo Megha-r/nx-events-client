7e2aeac52d498aedf5749e927012d4dd6ca1cc47
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2021-02-12 18:49:05 +0530

    loginEmployee

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,7 +3,7 @@ import {
 } from 'react';
 
 const Overview = lazy(() => import('./Overview'));
-const Query = lazy(() => import('../pages/event/EventList'));
+const Query = lazy(() => import('../modules/query'));
 const Wrapper = lazy(() => import('../pages/login/Wrapper'));
 
 export {
